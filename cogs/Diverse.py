516b0552d443a31e55d972af27fe78f7f4649c8c
Hollitizz <valentin.caure@epitech.eu>
2022-11-16 15:21:08 +0100

    [ADD] auto helper

--- cogs/Diverse.py
+++ cogs/Diverse.py
@@ -18,6 +18,10 @@ class Diverse(commands.Cog, description="Groupe de commande Divers"):
         _logger.info(f"Commande ping exécutée par {ctx.user}")
         await ping.ping(ctx)
 
+    @app_commands.command(name="help", description="Display help menu")
+    async def help(self, ctx: Interaction):
+        await help.help(self.bot, ctx)
+
 
 async def setup(bot: Setup):
     await bot.add_cog(Diverse(bot), guilds=[Object(id=bot.guild_id)])
